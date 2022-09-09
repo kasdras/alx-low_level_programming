6e7bfab0fff8894d4bf9ddf2b4d41223424773c4
Idris Kazeem <kazdraz0@gmail.com>
2022-09-08 17:58:13 -0700

    printing strings using puts() function, char my_str [] = string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*
+ * Initialize a character or string data type
+ *
+ * the puts() function prints the string
+ * and the puts() comes with a new line
+ *
+ */
+
+int main()
+{
+	char my_str[] = "Programming is like building a multilingual puzzle";
+	puts(my_str);
+	return(0);
+}
+
+
