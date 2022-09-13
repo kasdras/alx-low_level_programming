5b78390de9b2606aece6ccdf018c184d8dfade05
Idris Kazeem <kazdraz0@gmail.com>
2022-09-13 14:08:46 -0700

    prints putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - calls the putchar ()
+ *
+ * Return: Always 0 Success
+ */
+
+int main(void)
+{
+	_putchar();
+	return 0;
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+It is about variables, functions and nested loops
