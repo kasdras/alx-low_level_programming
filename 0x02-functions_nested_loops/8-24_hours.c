3d01d999f2f29fa12c87649eb9507da953a30571
Idris Kazeem <kazdraz0@gmail.com>
2022-09-14 03:42:19 -0700

    changed char to int

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,21 +10,21 @@
  */
 void jack_bauer(void)
 {
-	char m, n, o, p;
+	int m, n, o, p;
 
-	for (m = '0'; m <= '2'; ++m)
+	for (m = 0; m <= 2; ++m)
 	{
-		for (n = '0'; n <= '3'; n++)
+		for (n = 0; n <= 3; n++)
 		{
-			for (o = '0'; o <= '5'; o++)
+			for (o = 0; o <= 5; o++)
 			{
-				for (p = '0'; p <= '9'; p++)
+				for (p = 0; p <= 9; p++)
 				{
-					_putchar(m);
-					_putchar(n);
+					_putchar(m + '0');
+					_putchar(n + '0');
 					_putchar(':');
-					_putchar(o);
-					_putchar(p);
+					_putchar(o + '0');
+					_putchar(p + '0');
 					_putchar('\n');
 				}
 			}
