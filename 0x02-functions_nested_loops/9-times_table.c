c1d72a2be1e8cf1ad3ba58b7726a4b47bdaa416a
Idris Kazeem <kazdraz0@gmail.com>
2022-09-14 05:58:47 -0700

    9-times_table prints the times table from 0 to 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * times_table -prints the times table from 0 to 9.
+ *
+ * Return: Nothing
+ */
+void times_table(void)
+{
+	int m, n, p;
+
+	for (m = 0; m <= 9; m++)
+	{
+		for (n = 0; n <= 9; n++)
+		{
+			p = (m * n);
+			if (n != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (p >= 10)
+			{
+				_putchar((p / 10) + '0');
+				_putchar((p % 10) + '0');
+			}
+			else if (p < 10 && n != 0)
+			{
+				_putchar(' ');
+				_putchar((p % 10) + '0');
+			}
+			else
+				_putchar((p % 10) + '0')
+		}
+		_putchar('\n');
+	}
+}
