71d2db470a04d80d288c091621352dc3a222b6d2
Idris Kazeem <kazdraz0@gmail.com>
2022-09-13 18:32:59 -0700

    adds two integers and returns the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - checks for alphabetic character
+ *
+ * int: it is a sign
+ * @p: it is a value
+ * @c: a value
+ *
+ * Return: Always 0 Success
+ */
+int add(int p, int c)
+{
+	int m;
+
+	m = p + c;
+	return (m);
+}
