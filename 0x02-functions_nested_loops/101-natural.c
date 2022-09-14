d4fceb7073ac7f80966e8458817ca6549c03ee6e
Idris Kazeem <kazdraz0@gmail.com>
2022-09-14 06:29:25 -0700

    main in 101-natural computes and prints the sum of all the multiples of 3 or 5 below 1024

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - compute and prints the sum of all the multiples 
+ * 3 or 5 below 1024
+ * Return: void
+ */
+int main(void)
+{
+	int m, n;
+
+	for (m = 0; m < 1024; m++)
+		if (m % 3 == 0 || m % 5 == 0)
+			p += m;
+	printf("%d\n", p);
+	return(0);
