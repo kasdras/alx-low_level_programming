0df37b85950c03bf185df6f8ca055a61d7f7ab73
Idris Kazeem <kazdraz0@gmail.com>
2022-09-14 00:42:28 -0700

    checking 90

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -14,15 +14,15 @@ int print_last_digit(int p)
 {
 	int m;
 
-	if ((p > 0) || (p == print_last_digit(INT_MIN)))
+	if ((p < 0) || (p == print_last_digit(INT_MIN)))
 	{
 		m = -1;
 		p = m * p;
 	}
 	while (p > 10)
 		p = p % 10;
-	_putchar('0' + p);
-	_putchar('0' + p);
+	_putchar(p + '0');
+	_putchar(p + '0');
 	_putchar('\n');
 	return (1);
 }
