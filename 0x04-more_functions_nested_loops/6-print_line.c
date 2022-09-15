554d5f41a090c341dbb48bd711656d83cb2532fa
Idris Kazeem <kazdraz0@gmail.com>
2022-09-15 16:40:21 -0700

    draws a straight line in the terminal

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,7 +2,7 @@
 
 /**
  * print_line - draws a straight line in the terminal
- *@n the number of times the character _ should be printed
+ * @n: the number of times the character _ should be printed
  * Return: prints _ n times
  */
 
