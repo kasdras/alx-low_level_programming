96533cb1a3e5268721896e04903c059711ce2849
Idris Kazeem <kazdraz0@gmail.com>
2022-09-15 06:46:31 -0700

    just adding two untracked files

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - print positive and negative numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int alp, m, n;
+
+	for (alp = '0'; alp <= '9'; ++alp)
+	{
+		for (m = '0'; m <= '9'; m++)
+		{
+			if (alp < m)
+			{
+				putchar(alp);
+				putchar(m);
+				putchar(' ');
+			}
+		}
+	}
+	for (n = '0'; n <= '9'; ++n)
+			for (n = '0'; n <= '9'; n++)
+			{
+				if ((alp < n && alp < m) && (m < n))
+				{
+					putchar(alp);
+					putchar(m);
+					putchar(n);
+					if (alp == '7' && n == '9')
+						putchar('\n');
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	return (0);
+}
