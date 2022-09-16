7a40ef1fb5e49bfc4e4970d7d33468da5d1399bf
Idris Kazeem <kazdraz0@gmail.com>
2022-09-15 17:54:19 -0700

    prints number 1 to 100, and fizz buzz for multiples of 3 and 5

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,22 +14,16 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3 == 0) && (i % 5 == 0))
-		{
-			printf("FizzBuzz ");
-			continue;
-		}
+		if (i % 15 == 0)
+			printf("FizzBuzz");
 		else if (i % 3 == 0)
-		{
-			printf("Fizz ");
-			continue;
-		}
+			printf("Fizz");
 		else if (i % 5 == 0)
-		{
-			printf("Buzz ");
-			continue;
-		}
-		printf("%i ", i);
+			printf("Buzz");
+		else
+			printf("%i", i);
+		if (i < 100)
+			printf(" ");
 	}
 	printf("\n");
 	return (0);
