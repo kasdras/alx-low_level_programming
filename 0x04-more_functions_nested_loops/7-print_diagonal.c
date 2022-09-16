df87dc4861a289d20d7cce18e952b3667a779f34
Idris Kazeem <kazdraz0@gmail.com>
2022-09-15 17:00:11 -0700

    raws a diagonal line on the terminal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,14 +12,11 @@ void print_diagonal(int n)
 
 	if (n <= 0)
 		_putchar('\n');
-	else
+	for (i = 0; i <= n; i++)
 	{
-		for (i = 0; i <= n; i++)
-		{
-			for (k = 0; k < i; k++)
-				_putchar(' ');
-			_putchar('\\');
-			_putchar('\n');
-		}
+		for (k = 0; k < i; k++)
+			_putchar(' ');
+		_putchar('\\');
+		_putchar('\n');
 	}
 }
