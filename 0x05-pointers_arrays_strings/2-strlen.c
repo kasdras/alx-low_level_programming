826b5f0747804b153ff0d4b5b19e124401a94ef4
Idris Kazeem <kazdraz0@gmail.com>
2022-09-19 09:35:26 -0700

    returns the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - a function that swaps the values of two intgers
+ * @s: the string length to return
+ * Return: the length of the string
+ */
+
+int _strlen(char *s)
+{
+	int i, c;
+
+	c = 0;
+
+	for (i = 0; i <= s[]; i++)
+		c += i;
+	return (c);
+}
