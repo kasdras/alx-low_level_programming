b65fd9dc559f5fff62ce6fb8fd40aa820199da72
Idris Kazeem <kazdraz0@gmail.com>
2022-09-19 10:06:42 -0700

    prints a strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - a function that prints a string
+ * @str: the string to print
+ * Return: str
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+	while (*(s + i) != '\0')
+		_putchar(*(s + i));
+	_putchar('\n');
+
+}
