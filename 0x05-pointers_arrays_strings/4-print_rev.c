05cea8f36ecf80747affb1848fde6829ddf6b860
Idris Kazeem <kazdraz0@gmail.com>
2022-09-19 11:58:20 -0700

    prints strings in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -13,9 +13,10 @@ void print_rev(char *s)
 	i = 0;
 	while (*(s + i) != '\0')
 		i++;
+	i -= 1;
 	while (i >= 0)
 	{
-		_putchar(*(s + (i - 1)));
+		_putchar(*(s + i));
 		i--;
 	}
 	_putchar('\n');
