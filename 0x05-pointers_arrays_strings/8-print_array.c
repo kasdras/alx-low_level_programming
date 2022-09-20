a28c2c6b279b18a4a2e82d4b8326a89342a78028
Idris Kazeem <kazdraz0@gmail.com>
2022-09-19 19:50:24 -0700

    removed putchar to print comma

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -15,13 +15,10 @@ void print_array(int *a, int n)
 	i = 0;
 	while (i < n)
 	{
-		printf("%d", a[i]);
-		if (i != n - 1)
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
+		printf("%d,", a[i]);
 		i++;
+		if (i != n - 1)
+			_putchar(' ');
 	}
 	_putchar('\n');
 
