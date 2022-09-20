f79b78ae3dc4f194918e51450b85605b099c15ef
Idris Kazeem <kazdraz0@gmail.com>
2022-09-19 20:08:58 -0700

    n element of array of int

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -15,14 +15,14 @@ void print_array(int *a, int n)
 	i = 0;
 	while (i < n)
 	{
-		printf("%d", a[i]);
-		if (i != n - 1)
+		if (i < n)
 		{
-			printf(',');
-			printf(' ');
+			printf("%d", a[i]);
+			_putchar(',');
 		}
+		if (i != n - 1)
+			_putchar(' ');
 		i++;
 	}
 	_putchar('\n');
-
 }
