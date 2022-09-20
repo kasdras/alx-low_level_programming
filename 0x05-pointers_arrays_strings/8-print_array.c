acfc3365b66ebe5e5f3c29be83ae5b5944e4c7a7
Idris Kazeem <kazdraz0@gmail.com>
2022-09-19 19:38:05 -0700

    prints n element of an array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -15,11 +15,12 @@ void print_array(int *a, int n)
 	i = 0;
 	while (i < n)
 	{
-		printf("%d[i]", *a);
-		_putchar(',');
-		i++;
+		printf("%d[%d]", *a, i);
 		if (i != n - 1)
+		{
+			_putchar(',');
 			_putchar(' ');
+		}
 	}
 	_putchar('\n');
 
