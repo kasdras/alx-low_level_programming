ec5fdb43c516ebe6b8bdc26fcccaa08b225e1e8b
Idris Kazeem <kazdraz0@gmail.com>
2022-09-19 19:54:12 -0700

    put the increment after if statement

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -15,10 +15,13 @@ void print_array(int *a, int n)
 	i = 0;
 	while (i < n)
 	{
-		printf("%d,", a[i]);
-		i++;
+		printf("%d", a[i]);
 		if (i != n - 1)
+		{
+			_putchar(',');
 			_putchar(' ');
+		}
+		i++;
 	}
 	_putchar('\n');
 
