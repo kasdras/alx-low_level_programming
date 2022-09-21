bcf872aca4f341c8920cccb1700cb10f3f272e78
Idris Kazeem <kazdraz0@gmail.com>
2022-09-21 14:51:44 -0700

    function works like strcmp which compares the strings using ASCII value

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,28 +9,14 @@
 
 char _strcmp(char *s1, char *s2)
 {
-	int i, j, g, l, e;
+	int i;
 
 	i = 0;
-	j = 0;
-	for (i = 0; s1[i] == '\0'; i++)
+	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
 	{
-		for (j = 0; s2[j] == '\0'; j++)
-		{
-			if (s1[i] > s2[j])
-			{
-				g++;
-			}
-			else if (s1[i] < s2[j])
-				l--;
-			else
-				e = 0;
-		}
+		s1++;
+		s2++;
 	}
-	if (g > 0)
-		return (15);
-	else if (l < 0)
-		return (-15);
-	else
-		return (e);
+	i = (*s1 == *s2)? 0: (*s1 > *s2)? 15: -15;
+	return i;
 }
