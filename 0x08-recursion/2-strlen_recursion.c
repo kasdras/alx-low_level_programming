ddc78c965c96e6dbf22426ca13845fcbf386b162
Idris Kazeem <kazdraz0@gmail.com>
2022-09-29 03:40:22 -0700

    returns the length of string using recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,11 +7,9 @@
  * Return: nothing
  */
 
-void _strlen_recursion(char *s)
+int _strlen_recursion(char *s)
 {
-	if (*s != 0)
-	{
-		return (1 + (_strlen_recursion(s + 1)));
-	}
-	return (0);
+	if (*s == 0)
+		return (0);
+	return (1 + (_strlen_recursion(s + 1)));
 }
