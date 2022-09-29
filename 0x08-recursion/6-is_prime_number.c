871d6f8c1325e0add6ba86d5e4fab6cf26e3c2e6
Idris Kazeem <kazdraz0@gmail.com>
2022-09-29 04:00:14 -0700

    checks if the inout integer is a prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * is_prime_number - checks if input integer is a prime number
+ * @n: the integer to check
+ * Return: 1 if prime number otherwise 0
+ */
+int is_prime_number(int n)
+{
+	if (n % 2 == 0)
+		return (0);
+	else if(n == 1)
+		return (1);
+	return (n / is_prime_number(n - 1));
+}
