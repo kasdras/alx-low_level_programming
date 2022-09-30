0c5e7d373e9f3aedbb985b9b0814c6e26c7d9e6d
Idris Kazeem <kazdraz0@gmail.com>
2022-09-30 05:58:23 -0700

    prints the first line command argument

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the first command line argument
+ * @argc: The number of command line argument
+ * argv: the array of command line argument
+ * Return: O Always 
+ */
+int main(int argc, char* argv[])
+{
+	printf("%s\n", argv[argc - 1]);
+	return (0);
+}
