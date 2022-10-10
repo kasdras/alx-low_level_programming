38a2fb06a84aa2bbe4620e394db22ec34c3d6b95
Idris Kazeem <kazdraz0@gmail.com>
2022-10-10 12:14:18 -0700

    it is all about structures and typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct dog - a struct with 3 properties
+ * @name: first
+ * @age: second
+ * @owner: third
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif /** MAIN_H **/
