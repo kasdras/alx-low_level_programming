c25dc8e944ed512c8a03fe5fafbd12576557e454
Idris Kazeem <kazdraz0@gmail.com>
2022-10-12 15:12:29 -0700

    prints the opcodes

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,39 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <udis86.h>
 
 /**
  * main - prints the opcodes
- * @argc: the length of argument
+ * @argc: the length of argv
  * @argv: the array of arguments
  *
- * Return: the opcodes
+ * Return: Always 0 on success
  */
+
 int main(int argc, char *argv[])
 {
-	ud_t ud_obj;
-	int j = 0; i = 0;
-
-	if (argc == 2)
+	if (argc != 2)
 	{
-		j = atoi(argv[1]);
-
-		if (j < 0)
-		{
-			printf("Error\n");
-			exit(2);
-		}
-
-		ud_unit(&ud_obj);
-		ud_set_input_buffer(&ud_obj, argv[1], j);
-		ud_set_mode(&ud_obj, 64);
-		ud_set_syntax(&ud_obj, UD_SYN_INTEL);
-
-		while (ud_disassemble(&ud_obj))
-		{
-			printf("\t%s\n", ud_insn_hex(&ud_obj));
-		}
+		printf("Error\n");
+		exit(1);
 	}
 
+	if (atoi(argv[1]) < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
 	return (0);
 }
