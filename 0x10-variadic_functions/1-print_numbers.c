53965b5108137b8d94deef5af41d703ded61805a
Idris Kazeem <kazdraz0@gmail.com>
2022-10-13 14:53:17 -0700

    prints the actual arguments

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,19 +13,20 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list args;
 
-	if (separator == NULL)
+	va_start(args, n);
+
+	for (i = 1; i <= n; i++)
 	{
-		;
-	}
-	else
-	{
-		va_start(args, n);
-		for (i = 0; i < n; i++)
+		printf("%i", va_arg(args, int));
+		if (i < n && separator)
 		{
-			printf("%d", va_arg(args, int));
 			printf("%s", separator);
 		}
-		printf("\n");
-		va_end(args);
+		else
+		{
+			;
+		}
 	}
+	printf("\n");
+	va_end(args);
 }
