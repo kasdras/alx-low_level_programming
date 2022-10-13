e17e02931f327ffb459299f66ef533806a02e0a7
Idris Kazeem <kazdraz0@gmail.com>
2022-10-13 14:46:39 -0700

    prints the actual arguments

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -24,15 +24,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		va_start(args, n);
 		for (i = 0; i < n; i++)
 		{
-			if (i == n - 1)
-			{
-				printf("%d\n", va_arg(args, int));
-			}
-			else
-			{
-				printf("%d%c ", va_arg(args, int), b);
-			}
+			printf("%d", va_arg(args, int));
+			printf("%c", b);
 		}
+		printf("\n");
 		va_end(args);
 	}
 }
