00ee3e6166ad5081d4fef2ba7bab5cdeaed8c7d3
Idris Kazeem <kazdraz0@gmail.com>
2022-11-04 03:57:57 -0700

    wrong use of a variable corrected

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,7 +18,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	buff = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
+	if (buff == NULL)
 		return (0);
 
 	o = open(filename, O_RDONLY);
