a95ba58f2a621fa178e84cdd953ed6a315515eb7
Idris Kazeem <kazdraz0@gmail.com>
2022-11-04 04:54:17 -0700

    removed whitespaces and space between the function close

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,14 +19,13 @@ int create_file(const char *filename, char *text_content)
 		for (i = 0; text_content[i];)
 			i++;
 	}
-	
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(o, text_content, i);
 
 	if (o == -1 || w == -1)
 		return (-1);
 
-	close (o);
+	close(o);
 
 	return (1);
 }
